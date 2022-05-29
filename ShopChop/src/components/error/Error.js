a250e62aa632e0c183701d0ee0240ab61c1ccc65
hale tosun <93587234+haletosun3@users.noreply.github.com>
2022-05-29 17:16:26 +0300

    Delete Error.js

--- ShopChop/src/components/error/Error.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import LottieView from "lottie-react-native";
-function Error(){
-    return <LottieView  source={require('../../assets/error-dialog.json')} autoPlay />
-}
-export default Error;
